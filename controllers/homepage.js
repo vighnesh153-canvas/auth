d45997484a91ab0b8eaca9befa396c47e5dfa579
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-01 15:37:14 +0530

    add the homepage and status controller

---
+++ controllers/homepage.js
@@ -0,0 +1,3 @@
+module.exports = (req, res) => {
+    res.send("<h1>This is my auth service</h1>")
+};
--- routes.js
+++ routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const homePageController = require('./controllers/homepage');
+const statusController = require('./controllers/status');
+
 const signUpController = require('./controllers/sign-up');
 const confirmSignUpController = require('./controllers/confirm-sign-up');
 const loginController = require('./controllers/login');
 const verifyTokenController = require('./controllers/verify-token');
+
+router.get('/', homePageController);
+router.get('/status', statusController);
 
 router.post('/sign-up', signUpController);
 router.post('/confirm-sign-up', confirmSignUpController);
